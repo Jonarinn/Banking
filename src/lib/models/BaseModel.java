39d2ec75b2c31ee5bd325895945fceb9b76f10e8
Jonarinn <runarssonjon@gmail.com>
2026-04-26 18:41:02 +0200

    db mapping start

--- src/lib/models/BaseModel.java
+++ src/lib/models/BaseModel.java
@@ -2,5 +2,14 @@ package lib.models;
 
 import jakarta.persistence.*;
 
+@MappedSuperclass
 public class BaseModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long recordId;
+
+    public Long getRecordId()
+    {
+        return recordId;
+    }
 }
